09db611025e39cf38a5cf455413eb342910a0baa
sam bacha <sam@freighttrust.com>
2021-03-27 09:37:18 -0700

    feat(ts): tsdocs

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,8 +1,19 @@
+/**
+* @file Provider
+* @summary RPC Provider
+*/
+
 import { JsonRpcProvider } from '@ethersproject/providers';
 import networks from '../networks.json';
 
+/** @const providers */
 const providers = {};
 
+/**
+* @exports getProvider
+* @const url
+* @type {string}
+*/
 export default function getProvider(network: string) {
   const url: string = networks[network].rpc[0];
   if (!providers[network]) providers[network] = new JsonRpcProvider(url);
