aa49a815eb5d109ae87fa15a875f91d81a4bf951
sam bacha <sam@freighttrust.com>
2021-03-27 09:48:52 -0700

    feat(ts): tsdoc

--- src/utils/multicaller.ts
+++ src/utils/multicaller.ts
@@ -1,6 +1,17 @@
- import { JsonRpcProvider } from '@ethersproject/providers';
+/**
+* @file MulitCaller
+* @summary MulitCaller enables atomic JSON-RPC requests for EVM chains
+* 
+*/
+
+import { JsonRpcProvider } from '@ethersproject/providers';
 import set from 'lodash.set';
 import { multicall } from '../utils';
+
+/**
+* @class Multicaller
+* @param {string} network
+*/
 
 export default class Multicaller {
   public network: string;
@@ -9,7 +20,14 @@ export default class Multicaller {
   public options: any = {};
   public calls: any[] = [];
   public paths: any[] = [];
-
+ 
+/**
+* Multicaller Constructor
+* @constructor
+* @param {string} network - the blockchain network
+* @param JsonRpcProvider - JSON RPC Provider
+* @param {object} abi - ABI artifact 
+*/
   constructor(
     network: string,
     provider: JsonRpcProvider,
