4a05feea346a6b286cd55e4f1cfd8ffe9059d643
sam <sam@freighttrust.com>
2021-04-04 20:04:05 -0700

    feat(pkg): libcaller
    Signed-off-by: sam <sam@freighttrust.com>

---
+++ packages/libcaller/.elsintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint']
+};
---
+++ packages/libcaller/src/utils.ts
@@ -0,0 +1,11 @@
+export class Utils {
+  /**
+   * Deep clone a object
+   * @param object The object
+   */
+  public static deepClone<T>(object: T): T {
+    const cloned = JSON.parse(JSON.stringify(object)) as T;
+    // cloning causes data loss
+    return object;
+  }
+}
